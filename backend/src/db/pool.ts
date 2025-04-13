dbba54ec262b565a7a26b1c922a65c15b3c48989
mrkaye97 <mrkaye97@gmail.com>
2025-04-13 17:06:02 -0400

    fix: any

--- backend/src/db/pool.ts
+++ backend/src/db/pool.ts
@@ -2,12 +2,13 @@ import pg from "pg";
 
 export const pool = new pg.Pool({
   connectionString: "postgres://postgres:postgres@localhost:5442/medici",
-  max: 20, // Maximum number of clients
-  idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
-  connectionTimeoutMillis: 2000, // How long to wait for a connection
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
-export async function withConnection<T>(callback: (conn: any) => Promise<T>): Promise<T> {
+
+export async function withConnection<T>(callback: (conn: pg.PoolClient) => Promise<T>): Promise<T> {
   const conn = await pool.connect();
   try {
     return await callback(conn);
