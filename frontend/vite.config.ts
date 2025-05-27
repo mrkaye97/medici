3a764750a91ab3f18bfd7816d5a09b2544d8338e
mrkaye97 <mrkaye97@gmail.com>
2025-05-26 21:37:41 -0400

    fix: vite conf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,27 @@
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
 
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  preview: {
-    allowedHosts: ["localhost", process.env.VITE_HOST].filter(
-      (host): host is string => Boolean(host),
-    ),
-  },
+    preview: {
+      allowedHosts: ["localhost", env.VITE_HOST].filter(
+        (host): host is string => Boolean(host),
+      ),
+    },
+  };
 });
